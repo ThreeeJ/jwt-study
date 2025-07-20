35cc080768e5138fab41696bd1d8d9f44f293586
JongJin <2171266@hansung.ac.kr>
2025-07-20 17:50:00 +0900

    title : [스프링 시큐리티 JWT 11] JWT 검증 필터
    Reference: https://www.youtube.com/watch?v=7B6KHSZN3jY

--- SpringJWT/src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ SpringJWT/src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springjwt.config;
 
+import com.example.springjwt.jwt.JWTFilter;
 import com.example.springjwt.jwt.JWTUtil;
 import com.example.springjwt.jwt.LoginFilter;
 import lombok.RequiredArgsConstructor;
@@ -58,6 +59,10 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        // JWTFilter 등록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
         // 필터 추가 LoginFilter()는 인자를 받음
         // (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
